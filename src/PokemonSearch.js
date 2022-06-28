ad13272d0823bbb78730a540ab0b127cb2798cc1
kathrynzaro <zarokathryn@gmail.com>
2022-06-28 16:37:41 -0700

    pokedex

---
+++ src/PokemonSearch.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import PokemonList from './PokemonList';
+import { getPokemon } from './services/fetch-utils';
+
+export default function PokemonSearch() {
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemonQuery, setPokemonQuery] = useState('');
+
+  async function doLoad() {
+    const data = await getPokemon(pokemonQuery);
+
+    setPokemon(data.results);
+  }
+
+  useEffect(() => {
+    doLoad();
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await doLoad();
+
+    setPokemonQuery('');
+  }
+  return (
+    <div className='pokemon-search'>
+      <form onSubmit={handleSubmit}>
+        <input value={pokemonQuery} onChange={e => setPokemonQuery(e.target.value)} />
+        <button>search</button>
+      </form>
+      <PokemonList pokemon={pokemon} />
+    </div>
+  );
+}
