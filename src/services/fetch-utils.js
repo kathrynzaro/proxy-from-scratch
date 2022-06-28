2e282be27b92eb4cdfd721ba2cf28d01cd57c820
kathrynzaro <zarokathryn@gmail.com>
2022-06-28 16:39:07 -0700

    editing getPokemon fetch

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,5 +1,5 @@
 export async function getPokemon(filter) {
-  const rawData = await fetch(`http://localhost:8888/.netlify/functions/pokemon?pokeQuery=${filter}`);
+  const rawData = await fetch(`/.netlify/functions/pokemon?pokeQuery=${filter}`);
   const data = await rawData.json();
 
   return data;
